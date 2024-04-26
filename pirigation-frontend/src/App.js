5ff7b37aae63eab2a01704955a4065b0c790d163
charles <charles@charlescarden.com>
2024-04-26 11:53:26 -0500

    Fix app.js data fetch

--- pirigation-frontend/src/App.js
+++ pirigation-frontend/src/App.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
-	const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-	useEffect(() => {
-		fetch('/api/message')
-		.then(response => response.json())
-		.then(message => setMessage(data.message));
-	}, []);
+    useEffect(() => {
+        fetch('/api/message')
+        .then(response => response.json())
+        .then(data => setMessage(data.message)); // Changed 'message' to 'data' to avoid confusion
+    }, []);
 
-	return (
-		<div className="App">
-		<header className="App-header">
-		<p>
-		Message from the server: {message}
-		</p>
-		</header>
-		</div>
-	);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Message from the server: {message}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
